d0e2e3019342768aaf957d3b12f790aa60f0acc9
Thawatchai Piyawat <thawatchai@usablelabs.org>
2012-09-24 17:04:42 +0700

    add UI

--- code/i18n.js
+++ code/i18n.js
@@ -1,6 +1,12 @@
 var thai_i18n = {
-  'submit': 'จัดเก็บ'
+  'user-login-message': 'กรุณาเข้าระบบ GotoKnow ก่อน',
+  'journal-entry-title': 'คุณกำลังทำงานอะไรอยู่ มีอะไรน่าแบ่งปันบ้าง?',
+  'share-this-page': 'แบ่งปันหน้านี้',
+  'submit': 'เพิ่มอนุทินใหม่'
 }
 
 $.i18n.setDictionary(thai_i18n);
+$('#user-login-message')._t('user-login-message');
+$('#journal-entry-title')._t('journal-entry-title');
+$('#share-this-page')._t('share-this-page');
 $('#form-submit')._t('submit');
--- code/popup.js
+++ code/popup.js
@@ -1,3 +1,18 @@
+function markdown_link(title, url) {
+  return "[{{title}}]({{url}})".replace('{{title}}', title).replace('{{url}}', url);
+}
+
 $('.home-link').on('click', function(e) {
   chrome.tabs.create({'url': 'http://www.gotoknow.org/'});
+});
+
+$('#user-login-message').on('click', function(e) {
+  chrome.tabs.create({'url': 'http://www.gotoknow.org/user_session/new'});
+});
+
+$('#share-this-page-button').on('click', function(e) {
+  chrome.tabs.getSelected(null, function(tab) {
+    var c = $('#form-content');
+    c.val(c.val() + markdown_link(tab.title, tab.url));
+  });
 });
