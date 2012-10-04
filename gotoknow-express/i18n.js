8405e0b122c2f61af697f231e6c674514d98ef57
Thawatchai Piyawat <thawatchai@usablelabs.org>
2012-10-04 23:14:49 +0700

    v0.1.2

--- gotoknow-express/i18n.js
+++ gotoknow-express/i18n.js
@@ -4,7 +4,8 @@ var thai_i18n = {
   'share-this-page': 'แบ่งปันหน้านี้',
   'submit': 'เพิ่มอนุทินใหม่',
   'success-message': 'เพิ่มอนุทินเรียบร้อยแล้ว',
-  'error-message': 'ไม่สามารถเพิ่มอนุทินได้ โปรดลองอีกครั้ง'
+  'error-message': 'ไม่สามารถเพิ่มอนุทินได้ โปรดลองอีกครั้ง',
+  'markdown-message': 'เขียนแบบ Markdown ได้'
 }
 
 $.i18n.setDictionary(thai_i18n);
@@ -14,3 +15,4 @@ $('#share-this-page')._t('share-this-page');
 $('#form-submit')._t('submit');
 $('#success-message')._t('success-message');
 $('#error-message')._t('error-message');
+$('#markdown-message')._t('markdown-message');
